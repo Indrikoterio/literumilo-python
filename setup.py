e6e71ff825f33cc1ff20210c8807dbc51bae3ec8
Cleve <indriko@yahoo.com>
2020-05-11 18:26:36 +0900

    Include all modules.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Setup module for literumilo.
+# Setup module for literumilo package.
 # Cleve (Klivo) Lendon, 2020-05
 
 import setuptools
@@ -16,6 +16,12 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url="https://github.com/Indrikoterio/literumilo",
     packages = setuptools.find_packages(),
+    package_data = {'tests': ['test.txt'], 'data': ['vortaro.tsv', 'example.txt']},
+    include_package_data = True,
+    py_modules = ['literumilo', 'literumilo_check_word', 'literumilo_ending', \
+                             'literumilo_entry', 'literumilo_load', 'literumilo_morpheme_list', \
+                             'literumilo_scan_morphemes', 'literumilo_suffix', 'literumilo_utils',  \
+                             'example'],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
