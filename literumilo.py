43371809235d451c3da787f1a352b1ce4d4e498d
Cleve <indriko@yahoo.com>
2020-05-11 15:24:58 +0900

    New function, analyze_string().

--- literumilo.py
+++ literumilo.py
@@ -33,16 +33,19 @@ HOW_TO_USE = """\nLiterumilo   version: 1.0\n
 
 def analyze_file(filename, mode):
     """
-    Analyze the text within a file. If the mode is False, check the spelling
-    of each word in the file, and return a list of unknown words.
-    If the mode is True, return the analyzed text with each known word
-    divided into morphemes (separated by periods)..
+    This function reads text from a file and calls analyze_string(),
+    which does a morphological analysis or spell check on the text.
     Params:
         file name
         mode - True = morphological analyzer, False = spell checker
     Return:
         analyzed text, or list of misspelled words  (str)
     """
+
+    if not os.path.exists(filename):   # If there is no file.
+        print("Cannot find file: {}".format(filename))
+        sys.exit(0)
+
     # Read the file into a string.
     fin = open(filename, 'r')
     if not fin:
@@ -53,12 +56,30 @@ def analyze_file(filename, mode):
     s = fin.read()
     fin.close()
 
+    return analyze_string(s, mode)
+
+# ------------------------ analyze_file
+
+
+def analyze_string(text, mode):
+    """
+    Analyzes a string of Esperanto text. If the mode is False, this function
+    checks the spelling of each word and returns a list of unknown words.
+    If the mode is True, the function returns the analyzed text with each
+    known word divided into morphemes (separated by periods).
+    Params:
+        text
+        morpheme mode: True = morphological analyzer, False = spell checker
+    Return:
+        analyzed text, or list of misspelled words (str)
+    """
+
     bad_words = set()    # To output list of misspelled words in spell-check mode.
     new_text = ""   # To output analyzed text in morphological analysis mode (-m).
 
     in_word = False
     collected_chars = ""
-    for ch in s:   # Iterate over each character.
+    for ch in text:   # Iterate over each character.
         if is_word_char(ch):
             in_word = True
             collected_chars += ch
@@ -91,6 +112,8 @@ def analyze_file(filename, mode):
         for word in bad_words:
             bad_str += "{}\n".format(word)
         return bad_str
+
+# ------------------------ analyze_string
 
 def main(params):
 
